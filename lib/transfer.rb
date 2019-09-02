0c9b5a6e85947de448fd0e22a758fc8b9e25d5db
Stephen Blais <stephen@s-blais.com>
2019-09-02 21:58:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,12 +22,13 @@ class Transfer
     elsif sender.balance < @amount || !valid?
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
-    #elsif !valid?
-      #@status = "rejected"
-      #"Transaction rejected. Please check your account balance."
     end
   end
 
-
+  def reverse_transfer
+    if @status == "complete"
+      sender.balance += @amount
+      receiver.balance -= @amount
+      @status = "reversed"
 
 end
