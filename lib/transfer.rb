9a866cc1075729784a9ed0dc0df7da65a1b0f818
Stephen Blais <stephen@s-blais.com>
2019-09-02 21:46:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? and sender.balance >= @amount
+    if valid? && sender.balance >= @amount && @status = "pending"
       sender.balance -= @amount
       receiver.balance += @amount
       @status = "complete"
